48a070170ae356c3b8ddd8e47267c9fab2a5f0ca
Natalia Konovalova <ko.natalia.de@gmail.com>
2020-12-19 18:42:53 +0100

    moved function to modules and imported to index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if(!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-    return output;
-}
 
 // we read our data flile sync because only one time
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-let tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
@@ -28,11 +15,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 // ------CREATING SERVER
 const server = http.createServer((req, res)=> {
     // parsing our url to get pathname and id
-    const {search, query, pathname} = url.parse(req.url, true);
+    const {query, pathname} = url.parse(req.url, true);
 
     // creating router
     //-------- OVERVIEW
-    if(pathName == '/overview' || pathName == '/') {
+    if(pathname == '/overview' || pathname == '/') {
         res.writeHead(200, {'Content-type': 'text/html'});
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
         // console.log(cardsHtml);
@@ -41,14 +28,14 @@ const server = http.createServer((req, res)=> {
         res.end(output);
     }
     // --------PRODUCT PAGE
-    else if (pathName == `/product${search}`) {
+    else if (pathname == '/product') {
         res.writeHead(200, {'Content-type': 'text/html'});
         const product = dataObj[query.id];
-        const productHtml = replaceTemplate(tempProduct, product);
-        res.end(productHtml);
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
     }
     // --------API
-    else if (pathName == '/api') {
+    else if (pathname == '/api') {
         // we need to send to brauser this header to read content
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
